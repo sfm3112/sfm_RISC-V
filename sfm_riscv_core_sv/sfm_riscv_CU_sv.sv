33a24ee423bbb48cd84d7229ae97c8e65a77b7b0
sfm3112 <sfm3112@rit.edu>
2026-05-28 19:06:43 -0400

    Top level SV files, and Daily Log complete
    Created top level SV files, and PM_DM files, and completed daily log for 5-28

---
+++ sfm_riscv_core_sv/sfm_riscv_CU_sv.sv
@@ -0,0 +1,7 @@
+module sfm_riscv_CU_sv (
+	input logic clk,
+	output logic OPD);
+	
+	assign OPD = clk;
+	
+endmodule
---
+++ sfm_riscv_core_sv/sfm_riscv_DP_sv.sv
@@ -0,0 +1,7 @@
+module sfm_riscv_DP_sv (
+	input logic clk,
+	output logic OPD);
+	
+	assign OPD = clk;
+	
+endmodule
