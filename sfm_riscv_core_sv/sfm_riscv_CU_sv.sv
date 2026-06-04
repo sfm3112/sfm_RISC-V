b43627941f8275e77a035e1b0f3cfe5f88e5f7b8
sfm3112 <sfm3112@rit.edu>
2026-06-04 11:56:17 -0400

    Added constants to CU
    Constants added to CU

--- sfm_riscv_core_sv/sfm_riscv_CU_sv.sv
+++ sfm_riscv_core_sv/sfm_riscv_CU_sv.sv
@@ -1,7 +1,36 @@
-module sfm_riscv_CU_sv (
-	input logic clk,
-	output logic OPD);
-	
-	assign OPD = clk;
-	
-endmodule
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Data Path RISCV File
+	Created by: Stephen Meyer (6/4/2026)
+
+*///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module sfm_riscv_CU_sv # (parameter int WIDTH = 32)(input logic clk, rst, input logic [WIDTH-1:0]IW,
+																	output logic LDrd, LdPC, cntPC, LdIWR, WBsel, LdMAB, LdMAX, LdOPDR);
+
+/////////////////////////////////////////////////////////CONSTANTS//////////////////////////////////////////////////////////
+
+//------------------------------------------------------OPCODE TYPES------------------------------------------------------//
+
+localparam logic [6:0] TYPE_R  	=	7'b0110011;
+localparam logic [6:0] TYPE_L  	=	7'b0000011;
+localparam logic [6:0] TYPE_JR  	=	7'b1100111;
+localparam logic [6:0] TYPE_S  	=	7'b1100011;
+localparam logic [6:0] TYPE_B  	=	7'b1100011;
+localparam logic [6:0] TYPE_UI	=	7'b0110111;
+localparam logic [6:0] TYPE_UPC	=	7'b0010111;
+localparam logic [6:0] TYPE_J  	=	7'b1101111;
+
+//-----------------------------------------------------MACHINE CYCLES-----------------------------------------------------//
+
+localparam logic [1:0] MC0 = 2'b00;
+localparam logic [1:0] MC1 = 2'b01;
+localparam logic [1:0] MC2 = 2'b10;
+localparam logic [1:0] MC3 = 2'b11;
+
+///////////////////////////////////////////////////////CONTROL UNIT/////////////////////////////////////////////////////////
+
+
+
+
+
--- sfm_riscv_core_sv/sfm_riscv_core_sv.sv
+++ sfm_riscv_core_sv/sfm_riscv_core_sv.sv
@@ -5,17 +5,18 @@
 
 *///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-module sfm_riscv_core_sv # (parameter int WIDTH = 32)(input logic clk, output logic OPD);
+module sfm_riscv_core_sv # (parameter int WIDTH = 32)(input logic clk, rst, output logic OPD);
 	
 /////////////////////////////////////////////////////////WIRE NAMES/////////////////////////////////////////////////////////
 
 logic [WIDTH-1:0] IW;
+logic LDrd, LdPC, cntPC, LdIWR, WBsel, LdMAB, LdMAX, LdOPDR;
 
 ///////////////////////////////////////////////////COMPONENT INSTANTIATION//////////////////////////////////////////////////	
 	sfm_riscv_DP_sv #(.WIDTH(WIDTH)) DataPath (.clk(clk), .rst(rst), .IW(IW), .LDrd(LDrd), .LdPC(LdPC), .cntPC(cntPC), 
 															.LdIWR(LdIWR), .WBsel(WBsel), .LdMAB(LdMAB), .LdMAX(LdMAX), .LdOPDR(LdOPDR),
 															.OPD(OPD));
 
-	sfm_riscv_CU_sv #(.WIDTH(WIDTH)) ControlUnit ();
-	
+	sfm_riscv_CU_sv #(.WIDTH(WIDTH)) ControlUnit (.clk(clk), .rst(rst), .IW(IW), .LDrd(LDrd), .LdPC(LdPC), .cntPC(cntPC), 
+															.LdIWR(LdIWR), .WBsel(WBsel), .LdMAB(LdMAB), .LdMAX(LdMAX), .LdOPDR(LdOPDR));
 endmodule
