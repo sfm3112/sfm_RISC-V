67d15dfb9621a9fae20ad35775a2fa2bd9dd5e90
sfm3112 <sfm3112@rit.edu>
2026-06-05 12:09:25 -0400

    Combined CU updates + Daily Log complete
    Most of the day's systemVerilog work as well as the Daily log, Does not include weekly summary.

--- sfm_riscv_core_sv/sfm_riscv_CU_sv.sv
+++ sfm_riscv_core_sv/sfm_riscv_CU_sv.sv
@@ -15,6 +15,7 @@ module sfm_riscv_CU_sv # (parameter int WIDTH = 32)(input logic clk, rst, input 
 //------------------------------------------------------OPCODE TYPES------------------------------------------------------//
 
 localparam logic [6:0] TYPE_R  	=	7'b0110011;
+localparam logic [6:0] TYPE_I  	=	7'b0010011;
 localparam logic [6:0] TYPE_L  	=	7'b0000011;
 localparam logic [6:0] TYPE_JR  	=	7'b1100111;
 localparam logic [6:0] TYPE_S  	=	7'b0100011;
@@ -33,7 +34,20 @@ localparam logic [1:0] MC3 = 2'b11;
 logic [1:0] MC;
 logic [1:0] next_MC;
 
+//-----------------------------------------------------WB_SEL SIGNALS-----------------------------------------------------//
+
+localparam logic [2:0] IMM 		= 3'd0;		//NOTE: CURRENTLY NOT PROPERLY MAPPED, WILL BE FIXED SOON
+localparam logic [2:0] ALU_RES	= 3'd1;
+localparam logic [2:0] LD_WB 		= 3'd2;
+localparam logic [2:0] IW 			= 3'd3;
+localparam logic [2:0] INPUT 		= 3'd4;
+localparam logic [2:0] MAROUT		= 3'd5;
+localparam logic [2:0] PCOUT 		= 3'd6;
+localparam logic [2:0] NA 			= 3'd7;
+
 ///////////////////////////////////////////////////////CONTROL UNIT/////////////////////////////////////////////////////////
+
+//--------------------------------------------------CLOCK EDGE CONTROL----------------------------------------------------//
 
 always_ff @(posedge clk or posedge rst) begin
     if (rst)
@@ -42,49 +56,55 @@ always_ff @(posedge clk or posedge rst) begin
         MC <= next_MC;
 end
 
+//-----------------------------------------------COMBINATIONAL LOGIC BLOCKS-----------------------------------------------//
+
 always_comb begin
 	case (MC)
-	
+//-----------------------------------------------------------MC0----------------------------------------------------------//	
 		MC0 : begin
-			LdPC = 1'b0; cntPC = 1'b1; LDrd = 1'b0; LdIWR = 1'b1; WBsel = 2'b00; LdMAB = 1'b0; LdMAX = 1'b0; LdOPDR = 1'b0; next_MC = MC1;
+			LdPC = 1'b0; cntPC = 1'b1; LDrd = 1'b0; LdIWR = 1'b1; WBsel = NA; LdMAB = 1'b0; LdMAX = 1'b0; LdOPDR = 1'b0; next_MC = MC1;
 		end
 		
 //		MC1 : begin
 			//Experimenting if the "CU decodes IW" cycle is unnecessary
 //		end
-		
+//-----------------------------------------------------------MC1----------------------------------------------------------//		
 		MC1 : begin
 		case (IW[6:0])
 			TYPE_R : begin
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b1; LdIWR = 1'b0; WBsel = ALU_RES; LdMAB = 1'b0; LdMAX = 1'b0; LdOPDR = 1'b0; next_MC = MC0;
+			end
 			
+			TYPE_I : begin		//Same as TYPE_R
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b1; LdIWR = 1'b0; WBsel = ALU_RES; LdMAB = 1'b0; LdMAX = 1'b0; LdOPDR = 1'b0; next_MC = MC0;
 			end
 			
 			TYPE_L : begin
-			
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b0; LdIWR = 1'b0; WBsel = NA; LdMAB = 1'b1; LdMAX = 1'b1; LdOPDR = 1'b0; next_MC = MC2;
 			end
 			
-			TYPE_JR : begin
-			
+			TYPE_JR : begin	//Should be same as Type L mc1? If they are, could possibly be combined
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b0; LdIWR = 1'b0; WBsel = NA; LdMAB = 1'b1; LdMAX = 1'b1; LdOPDR = 1'b0; next_MC = MC2;
 			end
 			
-			TYPE_S : begin
-			
+			TYPE_S : begin		//Appears to be the same as L as well
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b0; LdIWR = 1'b0; WBsel = NA; LdMAB = 1'b1; LdMAX = 1'b1; LdOPDR = 1'b0; next_MC = MC2;
 			end
 			
-			TYPE_B : begin
-			
+			TYPE_B : begin		//Also same as L
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b0; LdIWR = 1'b0; WBsel = NA; LdMAB = 1'b1; LdMAX = 1'b1; LdOPDR = 1'b0; next_MC = MC2;
 			end
 			
 			TYPE_UI : begin
-			
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b1; LdIWR = 1'b0; WBsel = IMM; LdMAB = 1'b0; LdMAX = 1'b0; LdOPDR = 1'b0; next_MC = MC0;
 			end
 			
 			TYPE_UPC : begin
-			
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b0; LdIWR = 1'b0; WBsel = NA; LdMAB = 1'b1; LdMAX = 1'b1; LdOPDR = 1'b0; next_MC = MC2;
 			end
 			
-			TYPE_J : begin
-			
+			TYPE_J : begin		//Same as UPC
+				LdPC = 1'b0; cntPC = 1'b0; LDrd = 1'b0; LdIWR = 1'b0; WBsel = NA; LdMAB = 1'b1; LdMAX = 1'b1; LdOPDR = 1'b0; next_MC = MC2;
 			end
 			
 			default : begin
@@ -94,10 +114,16 @@ always_comb begin
 		endcase
 		end
 		
+//-----------------------------------------------------------MC2----------------------------------------------------------//	
+	
 		MC2 : begin
 		case (IW[6:0])
 			TYPE_R : begin
 			
+			end
+			
+			TYPE_I : begin
+				
 			end
 			
 			TYPE_L : begin
