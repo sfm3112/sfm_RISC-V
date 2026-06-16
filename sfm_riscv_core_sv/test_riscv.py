605bc68161487fe6e889f39d5ec79fdb593a14df
Stephen M <sfm3112@rit.edu>
2026-06-16 12:36:58 -0400

    Potentially finished the python script

--- sfm_riscv_core_sv/test_riscv.py
+++ sfm_riscv_core_sv/test_riscv.py
@@ -47,9 +47,31 @@ async def testA(dut):
 				if val < 0:		# Python can't compare a negative decimal to a negative binary 2'sC
 					val = (1 << 32) + val	# rotates the 1 to the maximum negative binary value, interpreted as very large decimal value, subtract the register value
 					
-				goodOutput.append((regNum, val, line.strip()))
+				goodOutput.append((regNum, val, line.strip()))	# sticks on the end of goodOutput the register number, the expected value, and the assembly line stripped of newline characters
 				
+	# Run the loop #
 	
-			
+	dut._log.info(f"Found {len(goodOutput)} assembly operations to run")	# printout of how long goodOutput is
+	
+	for regNum, expectedVal, originalLine in goodOutput:	# unpacks goodOutput
+		await waitForNextIW(dut)	# rruns the function from above
+		actualVal = int(dut.register_file.regs[regNum].value)	# sets "actualVal" to the value stored in the designated register
+		
+		assert actualVal == expectedVal, (f"\nFail on instruction: {originalLine}\n" f"Expected x{regNum} = {hex(expectedVal)} ({expectedVal})\n" f"Got x{regNum} = {hex(actualVal)} ({actualVal})" )
+		# If the actual value in the register doesn't match the expected value, print the failed TB statement
+		
+		dut._log.info("General test bench completed successfully.")	# Text printout
+		
+		dut._log.info("Testing branch loop.")	# Text printout
+		
+		await ClockCycles(dut.clk, 150)	# lets 150 clock cycles pass, program ends, to check the final result of the x1 and x2 registers
+		
+		final_x1 = int(dut.register_file.regs[1].value)	# store final result of x1
+		final_x2 = int(dut.register_file.regs[2].value)	# store final result of x2
+		
+		assert final_x1 == 337, f"Branch Loop Filed: x1 expected 337, got {final_x1}"
+		assert final_x2 == 337, f"Branch Loop Filed: x1 expected 337, got {final_x2}"
+	
+		dut._log.info("Branch test bench successful.")	# Final text printout
     
     pass
