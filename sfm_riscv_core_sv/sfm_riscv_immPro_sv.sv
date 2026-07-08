3875d512c88b3f2ccbaba647df15196f49c2be41
sfm3112 <sfm3112@rit.edu>
2026-07-08 17:20:30 -0400

    altered the immediate processor

--- sfm_riscv_core_sv/sfm_riscv_immPro_sv.sv
+++ sfm_riscv_core_sv/sfm_riscv_immPro_sv.sv
@@ -11,7 +11,12 @@ module sfm_riscv_immPro_sv # (parameter int WIDTH = 32)(input logic [WIDTH-1:0]I
 
 ///////////////////////////////////////////////////////INTERNAL LOGIC///////////////////////////////////////////////////////
 
-assign I = {{20{IW[31]}}, IW[31:20]};										//I type used in the ALU
+//if ({IW[13:12], IW[4]} == 3'b011) begin
+//	assign I = {27'd0, IW[24:20]};
+//end else begin
+//	assign I = {{20{IW[31]}}, IW[31:20]};										
+//end
+assign I = ({IW[13:12], IW[4]} == 3'b011) ? {27'd0, IW[24:20]} : {{20{IW[31]}}, IW[31:20]}; //I type used in the ALU
 assign S = {{20{IW[31]}}, IW[31:25], IW[11:7]};							//S type used in the store offset
 assign B = {{19{IW[31]}}, IW[7], IW[30:25], IW[11:8], 1'b0};		//B type used in branch jump offsets
 assign U = {IW[31:12], 12'b0};												//U type for upper 20 bits of address or register data 
